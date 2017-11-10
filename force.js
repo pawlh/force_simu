bfcd1ceac9d6e63feae6a486ad3662d945185605
Paul Hathaway <pawlh@users.noreply.github.com>
2017-11-10 10:00:19 -0700

    change resultant to negative resultant

--- force.js
+++ force.js
@@ -31,6 +31,7 @@ function calcResultant() {
     resultant.mag = pythag(resultant.x, resultant.y)
     resultant.angle = atand(resultant.y / resultant.x)
     resultant.angle = resultant.angle < 0 ? 360 + resultant.angle : resultant.angle
+    resultant.angle *= -1
     return resultant
 }
 
